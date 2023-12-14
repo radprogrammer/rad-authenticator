443be721914b498838e8afe593eb9a5e95e25a7d
corvus5 <patrickcebula@gmail.com>
2023-12-14 16:45:51 +0100

    TOTP: TimeStepWindow can be changed from the default 30 seconds

--- Source/radRTL.TOTP.pas
+++ Source/radRTL.TOTP.pas
@@ -14,10 +14,11 @@ type
   private const
     TimeStepWindow = 30; // 30 is recommended value. "The prover and verifier MUST use the same time-step"
   protected
-    class function GetCurrentUnixTimestamp: Int64;
+    class function GetCurrentUnixTimestamp(ATimeStepWindow: Integer): Int64;
   public
     /// <summary> TOTP: Time-Based One-Time Password Algorithm (most commonly used by Google Authenticaor)</summary>
-    class function GeneratePassword(const pBase32EncodedSecretKey: string; const pOutputLength: TOTPLength = TOTPLength.SixDigits): string; overload;
+    class function GeneratePassword(const pBase32EncodedSecretKey: string; const pOutputLength: TOTPLength = TOTPLength.SixDigits;
+      ATimeStepWindow: Integer = TimeStepWindow): string; overload;
   end;
 
 implementation
@@ -25,15 +26,16 @@ implementation
 uses
   System.DateUtils;
 
-class function TTOTP.GetCurrentUnixTimestamp: Int64;
+class function TTOTP.GetCurrentUnixTimestamp(ATimeStepWindow: Integer): Int64;
 begin
-  Result := DateTimeToUnix(Now, {AInputIsUTC=}False) div TTOTP.TimeStepWindow;
+  Result := DateTimeToUnix(Now, {AInputIsUTC=}False) div ATimeStepWindow;
 end;
 
 // https://datatracker.ietf.org/doc/html/rfc6238
-class function TTOTP.GeneratePassword(const pBase32EncodedSecretKey: string; const pOutputLength: TOTPLength = TOTPLength.SixDigits): string;
+class function TTOTP.GeneratePassword(const pBase32EncodedSecretKey: string; const pOutputLength: TOTPLength = TOTPLength.SixDigits;
+  ATimeStepWindow: Integer = TimeStepWindow): string;
 begin
-  Result := THOTP.GeneratePassword(pBase32EncodedSecretKey, GetCurrentUnixTimestamp, pOutputLength);
+  Result := THOTP.GeneratePassword(pBase32EncodedSecretKey, GetCurrentUnixTimestamp(ATimeStepWindow), pOutputLength);
 end;
 
 end.
