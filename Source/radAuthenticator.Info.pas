87a9272d690cd6a67222b0043bd3cf7665588d71
Darian Miller <darian@darianmiller.com>
2026-07-20 18:46:26 -0500

    Adding auto-inc support via Invoke-DelphiIncVer, part of https://github.com/continuous-delphi/delphi-powershell-ci

---
+++ Source/radAuthenticator.Info.pas
@@ -0,0 +1,11 @@
+unit radAuthenticator.Info;
+
+interface
+
+const
+  Version = '1.0.33';
+
+
+implementation
+
+end.
---
+++ tools/configure-hooks.bat
@@ -0,0 +1,4 @@
+:: Must be done once on each new system
+cd /d "%~dp0"
+git config core.hooksPath tools/hooks
+pause
